4934d85dd27635673b9dd07b4f2508eb096f0f22
keoniburns <keoni.burns@liatrio.com>
2023-06-13 13:34:00 -0700

    takes in cmdline args now

--- makeRepo.js
+++ makeRepo.js
@@ -1,36 +1,36 @@
 const { Octokit } = require("octokit");
 require('dotenv').config();
-const core = require('@actions/core');
-const fs = require('fs');
-const github = require('@actions/github');
+
 
 // const token = process.env.MY_TOKEN;
 const octokit = new Octokit({
     auth: process.env.MY_TOKEN,
 });
-function getFile(){
-    console.log("grabbing json file");
-    try {
-        fs.readFile(core.getInput('users'), (err, data) =>{
-            if(err) throw err;
-            const userDat = JSON.parse(data);
-            console.log(userDat);
-        })
-        console.log('json object from prev job: ', userDat);
-    }catch(e){
-        core.setFailed(e.message);
-    }
+// function getFile(){
+//     console.log("grabbing json file");
+//     try {
+//         fs.readFile(core.getInput('users'), (err, data) =>{
+//             if(err) throw err;
+//             const userDat = JSON.parse(data);
+//             console.log(userDat);
+//         })
+//         console.log('json object from prev job: ', userDat);
+//     }catch(e){
+//         core.setFailed(e.message);
+//     }
 
-    return userDat;
-}
+//     return userDat;
+// }
 
 
 //prolly grab only the necessary shit from here
-async function createRepo(userDat){
+async function createRepo(){
+    let userlogin = process.argv;
+    console.log(userlogin);
     try{
        let res = await octokit.graphql({
             query: `mutation{
-                createRepository(input:{name: "${userDat.userlog}", visibility: public}){
+                createRepository(input:{name: "${userlogin}", visibility: public}){
                     repository{
                         url
                         id
