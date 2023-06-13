e2c5ab0b737fc93c2d59cbee414dcd09133b0867
Carson Culler <carsonculler@admins-MacBook-Pro.local>
2023-06-13 11:14:57 -0700

    User scraping

---
+++ index.js
@@ -0,0 +1,52 @@
+import 'dotenv/config'
+import { Octokit, App } from "octokit"
+
+const octokit = new Octokit({ 
+    auth: process.env.TOKEN,
+});
+
+// Get number of users
+const numUsers = await octokit.graphql({query: `query MyQuery {
+    organization(login: "ImmortalHedgehogs") {
+        membersWithRole(first: 1) {
+            totalCount
+        }
+    }
+  }`,
+  headers: {
+  'X-GitHub-Api-Version': '2022-11-28',
+  }
+})
+
+// Get list of users
+const userData = await octokit.graphql({query: `query MyQuery {
+    organization(login: "ImmortalHedgehogs") {
+        membersWithRole(first: ${numUsers.organization.membersWithRole.totalCount}) {
+            nodes {
+                login
+            }
+        }
+    }
+  }`,
+  headers: {
+  'X-GitHub-Api-Version': '2022-11-28',
+  }
+})
+
+// Populate text file
+
+import fs from 'fs';
+import path from 'path';
+import { createWriteStream } from 'fs';
+import { format } from 'util';
+
+const logFile = path.join('.', 'output.json');
+const logStream = createWriteStream(logFile, { flags: 'w' });
+
+// Redirect console.log to the logStream
+console.log = function (message) {
+  logStream.write(format(message) + '\n');
+};
+
+// Usage
+console.log(userData.organization.membersWithRole.nodes);
